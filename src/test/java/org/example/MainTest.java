3f611d0ee91a335aba9545edc636345cd9bf80fb
solivalle <127651078+solivalle@users.noreply.github.com>
2023-12-27 16:41:17 -0600

    adding gradlew jar config

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
     @Test
     public void testMain(){
-        String filePath = "/sample.csv";
+        String filePath = "src/test/resources/sample.csv";
         String[] args = new String[1];
         args[0] = filePath;
         Main.main(args);
